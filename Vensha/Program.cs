27d55533173dbf0077c8f9f98cd3c27dae41086d
Venny <sgtmattis@gmail.com>
2020-08-18 12:33:49 +0200

    Initial commit

---
+++ Vensha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vensha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
